1daf8a4145bb7c507b65ce271cc9e74d11c179b6
Jose Saad <josemiguelsaad13@gmail.com>
2021-12-07 00:51:56 -0400

    animacion de logo de la ucab al login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,6 +10,7 @@ import {
   LockOutlined,
   EmailOutlined,
 } from "@mui/icons-material";
+import { motion } from "framer-motion";
 
 // ESTILOS
 const useStyles = makeStyles({
@@ -22,15 +23,13 @@ const useStyles = makeStyles({
     height: "200px",
     transition: "transform 1.1s ease-in-out",
     transitionDelay: "0.4s",
-    marginTop: "100pt",
   },
   registersvg: {
     width: "300px",
     height: "300px",
     transition: "transform 1.1s ease-in-out",
     transitionDelay: "0.4s",
-    marginTop: "160pt",
-    marginLeft: "400pt",
+    marginTop: "10%",
   },
   "@media (max-width: 870px)": {
     formsvg: {
@@ -63,8 +62,6 @@ const useStyles = makeStyles({
   
 });
 
-    
-
 const Login = () => {
   const classes = useStyles();
 
@@ -77,19 +74,42 @@ const Login = () => {
     myfunction();
   }
 
- const myfunction = () => {
-  const sign_in_btn = document.querySelector("#sign-in-btn");
-  const sign_up_btn = document.querySelector("#sign-up-btn");
-  const container = document.querySelector(".container");
-  
-  sign_up_btn.addEventListener("click", () => {
-    container.classList.add("sign-up-mode");
-  });
+  const myfunction = () => {
+    const sign_in_btn = document.querySelector("#sign-in-btn");
+    const sign_up_btn = document.querySelector("#sign-up-btn");
+    const container = document.querySelector(".container");
+    
+    sign_up_btn.addEventListener("click", () => {
+      container.classList.add("sign-up-mode");
+    });
 
-  sign_in_btn.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode");
-  });
- }
+    sign_in_btn.addEventListener("click", () => {
+      container.classList.remove("sign-up-mode");
+    });
+  }
+
+  const icon = {
+    hidden: {
+      opacity: 0,
+      pathLength: 0,
+      fill: "rgba(255, 255, 255, 0)"
+    },
+    visible: {
+      opacity: 1,
+      pathLength: 1,
+      fill: "#0185B6"
+    },
+    visible2: {
+      opacity: 1,
+      pathLength: 1,
+      fill: "#FDDF03"
+    },
+    visible3: {
+      opacity: 1,
+      pathLength: 1,
+      fill: "#00D49C"
+    }
+  };
 
   return (
     <div className={classes.containerLogin}>
@@ -97,7 +117,7 @@ const Login = () => {
         <div class="forms-container">
           <div class="signin-signup">
             <form action="#" class="sign-in-form">
-              <h2 class="title">Sign in</h2>
+              <h2 class="title">Iniciar Sesión</h2>
               <div class="input-field">
                 <PersonOutlined className={classes.inputIcon}/>
                 <input type="text" placeholder="Usuario" />
@@ -106,10 +126,10 @@ const Login = () => {
                 <LockOutlined className={classes.inputIcon}/>
                 <input type="password" placeholder="Contraseña" />
               </div>
-              <input type="submit" value="Login" class="btn solid" />
+              <input type="submit" value="Ingresar" class="btn solid" />
             </form>
             <form action="#" class="sign-up-form">
-              <h2 class="title">Sign up</h2>
+              <h2 class="title">Registrarse</h2>
               <div class="input-field">
                 <PersonOutlined className={classes.inputIcon}/>
                 <input type="text" placeholder="Usuario" />
@@ -122,7 +142,7 @@ const Login = () => {
                 <LockOutlined className={classes.inputIcon}/>
                 <input type="password" placeholder="Contraseña" />
               </div>
-              <input type="submit" class="btn" value="Sign up" />
+              <input type="submit" class="btn" value="Registrarse" />
             </form>
           </div>
         </div>
@@ -130,26 +150,119 @@ const Login = () => {
         <div class="panels-container">
           <div class="panel left-panel">
             <div class="content">
-              <h3>New here ?</h3>
+              <div className="container-svg-ucab-logo">
+                <motion.svg
+                  xmlns="http://www.w3.org/2010/svg"
+                  viewBox="0 0 139 128"
+                  className="item"
+                >
+                  <motion.path
+                    d="M69.5 19L88.9856 41.5V86.5L69.5 109L50.0144 86.5V41.5L69.5 19Z"
+                    stroke="#fff"
+                    stroke-width="3"
+                    fill="#0185B6"
+                    variants={icon}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{
+                      default: { duration: 6, ease: "easeInOut" },
+                      fill: { duration: 7, ease: [1, 0, 0.8, 1] }
+                    }}
+                  />
+                  <motion.path
+                    d="M114.5 19L133.986 41.5V86.5L114.5 109L95.0144 86.5V41.5L114.5 19Z"
+                    stroke="#fff"
+                    stroke-width="3"
+                    fill="#0185B6"
+                    variants={icon}
+                    initial="hidden"
+                    animate="visible3"
+                    transition={{
+                      default: { duration: 6, ease: "easeInOut" },
+                      fill: { duration: 7, ease: [1, 0, 0.8, 1] }
+                    }}
+                  />
+                  <motion.path
+                    d="M24.5 19L43.9856 41.5V86.5L24.5 109L5.01443 86.5V41.5L24.5 19Z"
+                    stroke="#fff"
+                    stroke-width="3"
+                    fill="#0185B6"
+                    variants={icon}
+                    initial="hidden"
+                    animate="visible2"
+                    transition={{
+                      default: { duration: 6, ease: "easeInOut" },
+                      fill: { duration: 7, ease: [1, 0, 0.8, 1] }
+                    }}
+                  />
+                </motion.svg>
+              </div>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-                ex ratione. Aliquid!
+                La mejor herramienta para creación, edición y manejo de encuestas.
+                <br/>
+                <br/>
+                ¿Eres nuevo? ¡Registrate aquí!
               </p>
               <button class="btn transparent" id="sign-up-btn" onClick={componentDidUpdate}>
-                Sign up
+                Registrarse
               </button>
             </div>
             <RegisterSVG class="image" className={classes.registersvg}/>
           </div>
           <div class="panel right-panel">
             <div class="content">
-              <h3>One of us ?</h3>
+              <div className="container-svg-ucab-logo">
+                <motion.svg
+                  xmlns="http://www.w3.org/2010/svg"
+                  viewBox="0 0 139 128"
+                  className="item"
+                >
+                  <motion.path
+                    d="M69.5 19L88.9856 41.5V86.5L69.5 109L50.0144 86.5V41.5L69.5 19Z"
+                    stroke="#fff"
+                    stroke-width="3"
+                    fill="#0185B6"
+                    variants={icon}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{
+                      default: { duration: 6, ease: "easeInOut" },
+                      fill: { duration: 7, ease: [1, 0, 0.8, 1] }
+                    }}
+                  />
+                  <motion.path
+                    d="M114.5 19L133.986 41.5V86.5L114.5 109L95.0144 86.5V41.5L114.5 19Z"
+                    stroke="#fff"
+                    stroke-width="3"
+                    fill="#0185B6"
+                    variants={icon}
+                    initial="hidden"
+                    animate="visible3"
+                    transition={{
+                      default: { duration: 6, ease: "easeInOut" },
+                      fill: { duration: 7, ease: [1, 0, 0.8, 1] }
+                    }}
+                  />
+                  <motion.path
+                    d="M24.5 19L43.9856 41.5V86.5L24.5 109L5.01443 86.5V41.5L24.5 19Z"
+                    stroke="#fff"
+                    stroke-width="3"
+                    fill="#0185B6"
+                    variants={icon}
+                    initial="hidden"
+                    animate="visible2"
+                    transition={{
+                      default: { duration: 6, ease: "easeInOut" },
+                      fill: { duration: 7, ease: [1, 0, 0.8, 1] }
+                    }}
+                  />
+                </motion.svg>
+              </div>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                laboriosam ad deleniti.
+                ¿Ya tienes cuenta? ¡Inicia sesión aquí!
               </p>
               <button class="btn transparent" id="sign-in-btn" onClick={componentDidUpdate}>
-                Sign in
+                Iniciar Sesión
               </button>
             </div>
             <FormSVG class="image" className={classes.formsvg}/>
