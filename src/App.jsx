a0517ffd3acb60118717bdde740f55a14bebc298
Jose Saad <josemiguelsaad13@gmail.com>
2021-12-12 22:55:26 -0400

    listo el drawer

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ import Table from './components/Table';
 function App() {
   return (
     <div className="App">
+      <Drawer/>
       <div style={{marginTop: '60pt'}}>
         <Table/>
         <Table/>
       </div>
-      <Drawer/>
+      
     </div>
   );
 }
