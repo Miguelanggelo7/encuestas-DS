0371b57e218e5b34fd50433919db56ce67882164
Hollow23-hub <borisbel231199@gmail.com>
2021-12-26 18:46:06 -0400

    actualizacion app

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import Drawer from './components/Menu/Drawer';
 import './components/Menu/drawer-styles.css';
 import Login from './pages/Login';
 import Table from './components/Table';
+import EncuestaRealizada from './components/Pantalla/Encuesta-realizada';
+import BarCharts from './components/graphics/BarCharts';
+import BasicSpeedDial from './components/Pantalla/SpeedDial';
+import IconButton from './components/Pantalla/buttonHome';
 
 function App() {
   return (
-    <div className="App">
-      <Drawer/>
-      <div id="blackground"/>
-      <div id="container-app">
-        <Table/>
-        <Table/>
-      </div>
-      
+    <div>
+      <IconButton/>
+      <EncuestaRealizada/>
+      <BasicSpeedDial/>
     </div>
   );
 }
 
 export default App;
+
+
+// <Drawer/>
+// <div id="blackground"/>
+// <div id="container-app">
+//   <Table/>
+//   <Table/>
+// </div>
