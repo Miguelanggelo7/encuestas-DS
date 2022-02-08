f599c1342c0467eefa6a428d0e5bd866ea14fa2a
Miguelanggelo Sumoza <miguelanggelo21@gmail.com>
2022-02-07 20:08:43 -0400

    Encuestas cargadas en MyForm

--- src/pages/MyForms.jsx
+++ src/pages/MyForms.jsx
@@ -1,33 +1,62 @@
 import React, { useEffect, useState } from "react";
+import { 
+    makeStyles,
+    Paper,
+    IconButton,
+    InputBase,
+    Divider
+} from "@material-ui/core";
+import {
+  Search
+} from "@mui/icons-material";
 import { getSurveysByUser, getCurrentUid } from "../firebase/functions";
-import GridMyForms from "../components/CardsForms/GridMyForms";
+
+// ESTILOS
+const useStyles = makeStyles({
+  
+});
 
 const MyForms = () => {
+  const classes = useStyles();
 
   // Si publicas y privadas = null, el usuario no ha creado ninguna encuesta
-  const [publicas, setPublicas] = useState(null);
-  const [privadas, setPrivadas] = useState(null);
+  const [surveys, setSurveys] = useState(null);
 
   useEffect(() => {
     const getData = async () => {
       const data = await getSurveysByUser(getCurrentUid());
       if (data) {
-        setPublicas(data.publicas);
-        setPrivadas(data.privadas);
+        setSurveys(Object.assign({}, data.publicas, data.privadas));
       }
     };
 
     getData();
   }, []); 
 
-  if (publicas && privadas) {
-    console.log(publicas)
-    console.log(privadas)
-  }
-
   return (
     <div style={{textAlign: 'center'}}>
-      <GridMyForms/>
+      <div style={{width: '220pt', margin: 'auto', marginTop: '60pt'}} >
+        <Paper
+          sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+          elevation={3}
+        >
+          <InputBase
+            placeholder="Buscar mis encuestas"
+            inputProps={{ 'aria-label': 'Buscar mis encuestas' }}
+          />
+          <IconButton aria-label="search">
+            <Search />
+          </IconButton>
+        </Paper>
+      </div>
+      <div>
+        {
+          surveys ? 
+          Object.keys(surveys).map((item) => <p>{item}</p>)
+          : 
+          <p>No hay nada flaco</p>
+        }
+      </div>
     </div>
   );
 };
