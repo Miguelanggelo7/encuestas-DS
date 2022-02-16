05a7301cba5eaa24e782b277d0700daed2beae9b
Jose Saad <josemiguelsaad13@gmail.com>
2022-02-16 15:38:39 -0400

    por ahora

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,19 @@ function App() {
                     <Drawer />
                     <SearchForms/>
                   </Route>
+                  <Route exact path="/respuestas" auth>
+                    <Drawer />
+                    <Table style={{margin: '50pt'}} 
+                      title={"Datos de prueba"}
+                      columns={[
+                        { title: 'Adı', field: 'name' },
+                        { title: 'Soyadı', field: 'surname' },
+                        { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+                        { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' } }
+                      ]}
+                      data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
+                    />
+                  </Route>
                   <Route path="/auth">
                     <Login/>
                   </Route>
